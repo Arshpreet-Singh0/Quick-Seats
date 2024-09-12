4dc74855477e26445912ab8f95217046775a45b2
Arshpreet Singh <arshsomal100@gmail.com>
2024-09-12 22:19:24 +0530

    about page

--- frontend/src/components/movieDetails/About.jsx
+++ frontend/src/components/movieDetails/About.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const About = ({movie}) => {
+const About = ({ movie }) => {
   return (
-    <div className='flex justify-center items-center p-10 bg-[#1a1a1a] h-full pb-24'>
-        <div className='w-[80%] text-white'>
-            <h1 className='text-3xl mb-2 opacity-95'>About : </h1>
-            <p className='opacity-90'>{movie.about}</p>
+    <div className="flex justify-center items-center  p-12 bg-[#1a1a1a] h-full  pb-24">
+      <div className="w-[60%] text-white">
+        <div className="mb-8">
+          <h1 className="text-3xl mb-2 opacity-95">About : </h1>
+          <p className="opacity-90">{movie.about}</p>
         </div>
-    </div>
-  )
-}
 
-export default About
+        <hr />
+
+        <div className="mt-5">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
