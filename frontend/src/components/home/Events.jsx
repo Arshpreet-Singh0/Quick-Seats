874c1ec47b2a0c7bd743a28ec75316ae8bf1617f
Arshpreet Singh <arshsomal100@gmail.com>
2024-09-16 23:59:40 +0530

    minor changes

--- frontend/src/components/home/Events.jsx
+++ frontend/src/components/home/Events.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Card from "../shared/Card";
 import { useSelector } from 'react-redux'
+import { useNavigate } from "react-router-dom";
 
 
 const Events = () => {
   const {allMovies} = useSelector(store=>store.movie);
-    console.log(allMovies);
+    const navigate = useNavigate();
   return (
     <>
       <div className="flex justify-between items-center mx-10 md:mx-14">
         <h1 className="text-4xl text-[white] mb-2 opacity-85">Popular </h1>
-        <h1 className="text-white opacity-65">Sell All <i className="fa fa-chevron-right"></i></h1>
+        <h1 className="text-white opacity-65 hover:opacity-100" onClick={()=>navigate("/allmovies")}>Sell All <i className="fa fa-chevron-right"></i></h1>
       </div>
       <div className=" w-full flex items-center justify-evenly flex-wrap p-5 ">
         {allMovies.slice(0,4).map((movie, idx) => {
