8e4c56b313ca4db70c53628ecdd9310e544217b6
Arshpreet Singh <arshsomal100@gmail.com>
2024-09-22 11:51:31 +0530

    admin theater table

---
+++ frontend/src/components/admin/AdminTheaters.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../shared/Navbar'
+import AllTheaters from './AllTheaters'
+
+const AdminTheaters = () => {
+  return (
+    <div>
+      <Navbar />
+      <AllTheaters />
+    </div>
+  )
+}
+
+export default AdminTheaters
